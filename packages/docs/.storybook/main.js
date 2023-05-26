83888d99a590a0fdfeaecbfc112f2b924511a342
Eldon Costa <eldoncosta1@gmail.com>
2023-05-26 10:45:08 -0300

    feat: configure accessbility addon

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,18 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
   }
-}
+};
