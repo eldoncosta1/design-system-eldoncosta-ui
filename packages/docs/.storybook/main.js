6e9a81b36605bd6eb3f7d8bff43fdfd59d910864
Eldon Costa <eldoncosta1@gmail.com>
2023-05-27 13:20:16 -0300

    fix: Change storybook version to 6

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,12 +1,15 @@
 module.exports = {
   "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
-  },
-  "features": {
-    "storyStoreV7": true
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
